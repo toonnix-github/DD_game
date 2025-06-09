7d5cb56f3fe35022c2fedde91f0ff53521d0fc72
Sagsun Disman <sagsun.d@fwd.com>
2025-06-10 02:49:50 +0700

    fix error

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -1,7 +1,4 @@
-        <div className="attack-value">
-          <img src="/add-icon.png" alt="attack" className="plus-icon" />
-          <span>{item.attack}</span>
-        </div>
+import React from 'react';
 import './ItemCard.css';
 
 function renderIcons(count, icon, alt) {
@@ -12,18 +9,24 @@ function renderIcons(count, icon, alt) {
 
 function ItemCard({ item }) {
   const typeIcons = {
-    melee: '/fist.png',
+    melee: '/fist.svg',
     magic: '/lightning.png',
     range: '/arrows.png',
   };
   const typeIcon = typeIcons[item.attackType || 'melee'];
   return (
     <div className="item-card">
+      <img className="item-image" src={item.image} alt={item.name} width="40" height="40" />
       <div className="item-name">{item.name}</div>
       <img className="item-image" src={item.image} alt={item.name} />
       <div className="item-stats">
         <div className="attack-icons">{renderIcons(item.attack, '/fist.svg', 'attack')}</div>
+        <div className="attack-value">
+          <img src="/add-icon.png" alt="attack" className="plus-icon" />
+          <span>{item.attack}</span>
+        </div>
         <div className="defence-icons">{renderIcons(item.defence, '/shield.svg', 'defence')}</div>
+        <span>{item.dice}</span>
       </div>
       <div className="weapon-type">
         <img src={typeIcon} alt={item.attackType || 'melee'} />
