d87c9f0783b5d2df670962a461d37e03206d5166
Sagsun Disman <sagsun.d@fwd.com>
2025-06-10 03:21:00 +0700

    Modify style

--- frontend/src/components/ItemCard.jsx
+++ frontend/src/components/ItemCard.jsx
@@ -16,17 +16,13 @@ function ItemCard({ item }) {
   const typeIcon = typeIcons[item.attackType || 'melee'];
   return (
     <div className="item-card">
-      <img className="item-image" src={item.image} alt={item.name} width="40" height="40" />
       <div className="item-name">{item.name}</div>
       <img className="item-image" src={item.image} alt={item.name} />
       <div className="item-stats">
-        <div className="attack-icons">{renderIcons(item.attack, '/fist.svg', 'attack')}</div>
         <div className="attack-value">
           <img src="/add-icon.png" alt="attack" className="plus-icon" />
           <span>{item.attack}</span>
         </div>
-        <div className="defence-icons">{renderIcons(item.defence, '/shield.svg', 'defence')}</div>
-        <span>{item.dice}</span>
       </div>
       <div className="weapon-type">
         <img src={typeIcon} alt={item.attackType || 'melee'} />
