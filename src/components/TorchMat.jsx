0f951573fd5053f04f2e3df27cfb52b56bdfa4d7
Sagsun Disman <sagsun.d@fwd.com>
2025-06-16 00:48:33 +0700

    Update torch image

--- src/components/TorchMat.jsx
+++ src/components/TorchMat.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import './TorchMat.scss'
+import React from 'react';
+import './TorchMat.scss';
 
 function TorchMat({ step, max = 20 }) {
   const cells = Array.from({ length: max + 1 }, (_, i) => (
@@ -7,10 +7,10 @@ function TorchMat({ step, max = 20 }) {
       key={i}
       className={`torch-cell${i === step ? ' current' : i < step ? ' spent' : ''}`}
     >
-      {i === step && <img src="/icon/torch.svg" alt="torch" />}
+      {i === step && <img src="/icon/torch.png" alt="torch" />}
     </div>
-  ))
-  return <div className="torch-mat">{cells}</div>
+  ));
+  return <div className="torch-mat">{cells}</div>;
 }
 
-export default TorchMat
+export default TorchMat;
