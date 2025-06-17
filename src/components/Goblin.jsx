c2d6360d78e4047b7bd90e16647e4eb34e2eb652
Sagsun Disman <sagsun.d@fwd.com>
2025-06-18 00:22:33 +0700

    Update goblin icon

--- src/components/Goblin.jsx
+++ src/components/Goblin.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Goblin.scss'
+import React from 'react';
+import './Goblin.scss';
 
 function Goblin({ goblin, damaged }) {
   return (
     <div className={`goblin${damaged ? ' shake' : ''}`}>
-      <img src={goblin.image} alt={goblin.name} />
+      <img src='/icon/icon-goblin.png' alt={goblin.name} />
     </div>
-  )
+  );
 }
 
-export default Goblin
+export default Goblin;
